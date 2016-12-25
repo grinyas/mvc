2869b3ec62ad582402bb8468dbfc878779264a8f
grinyas <grinyas00@gmail.com>
2016-12-25 10:29:41 +0200

    Delete vendor

--- www/index.php
+++ www/index.php
@@ -4,6 +4,3 @@ require_once '../config/bootstrap.php';
 
 $app = new \Academy\App();
 $app->run();
-
-// $config = require_once ROOT_PATH . '/config/config.php';
-// var_dump($config['database']['password']);
